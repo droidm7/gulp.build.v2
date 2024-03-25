8f26db71d7ef1c45219440901430a19c0b7d9a1f
droid664 <67774183+droid664@users.noreply.github.com>
2024-03-25 13:10:24 +0500

    Добавил fonts task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ import html from './gulp/tasks/html.js'
 import clean from './gulp/tasks/clean.js'
 import styles from './gulp/tasks/styles.js'
 import scripts from './gulp/tasks/scripts.js'
+import fonts from './gulp/tasks/fonts.js'
 
-gulp.task('build', gulp.parallel(html, styles, scripts))
+gulp.task('build', gulp.parallel(html, styles, scripts, fonts))
 gulp.task('default', gulp.series(clean, 'build'))
