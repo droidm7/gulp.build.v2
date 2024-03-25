edc488be5e34444faf7b12ff62af671a29a69d94
droid664 <67774183+droid664@users.noreply.github.com>
2024-03-25 15:20:29 +0500

    Добавил images task

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ import clean from './gulp/tasks/clean.js'
 import styles from './gulp/tasks/styles.js'
 import scripts from './gulp/tasks/scripts.js'
 import fonts from './gulp/tasks/fonts.js'
+import images from './gulp/tasks/images.js'
 
-gulp.task('build', gulp.parallel(html, styles, scripts, fonts))
+gulp.task('build', gulp.parallel(html, styles, scripts, fonts, images))
 gulp.task('default', gulp.series(clean, 'build'))
